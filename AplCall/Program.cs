cc8de75e6190765a7d309ccbc3f4da711df5f290
bryful <bryful@gmail.com>
2022-09-15 14:03:14 +0900

    プロジェクト ファイルを追加します。

---
+++ AplCall/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+#pragma warning disable CS8600
+namespace AplCall // Note: actual namespace depends on the project name.
+{
+	internal class Program
+	{
+		static string CallExePath()
+		{
+			string ret = "";
+			string fullName = Environment.ProcessPath;
+			string d = Path.GetDirectoryName(fullName);
+			string n = Path.GetFileNameWithoutExtension(fullName);
+			string e = Path.GetExtension(fullName);
+
+			string nn = n;
+			if (n.IndexOf("Call") >= 0) nn = n.Replace("Call", "");
+			else if (n.IndexOf("call") >= 0) nn = n.Replace("call", "");
+			else if (n.IndexOf("CALL") >= 0) nn = n.Replace("call", "");
+			if (nn == n) return ret;
+
+			ret = Path.Combine(d, nn + e);
+			return ret;
+		}
+		static void Main(string[] args)
+		{
+			string opt = "";
+			if (args.Length > 0)
+			{
+				for (int i = 0; i < args.Length; i++)
+				{
+					if (opt != "") opt += " ";
+					opt += "\"" + args[i] + "\"";
+				}
+			}
+
+			string n = CallExePath();
+			Console.WriteLine(n);
+			if (File.Exists(n) == false)
+			{
+				Console.WriteLine("[APleCall]");
+				Console.WriteLine("opt: "+ opt);
+				return;
+			}
+			else
+			{
+				using Process proc = new Process();
+				proc.StartInfo.FileName = n;
+				proc.StartInfo.Arguments = opt;
+				if (proc.Start())
+				{
+					//Console.WriteLine("exec!");
+				}
+				//Console.WriteLine("fin");
+				return;
+			}
+		}
+	}
+}
+#pragma warning restore CS8600
---
+++ README.md
@@ -0,0 +1,20 @@
+﻿# AplCall
+このアプリ名からCallを抜いた名前のアプリを実行させるDOSコマンドです。  
+  
+# Usage
+
+例えばFooCall.exeにリネームするとFoo.exeを呼び出すようになります。  
+引数は同じものを渡します。
+
+# License
+
+This software is released under the MIT License, see LICENSE. 
+
+# Authors
+
+bry-ful(Hiroshi Furuhashi) http://bryful.yuzu.bz/  
+twitter:[bryful](https://twitter.com/bryful)  
+bryful@gmail.com  
+
+# References
+
